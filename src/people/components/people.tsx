4b926eb815489cf5496b4271160c9932c5a1db46
Robin Ridderholt <robin.ridderholt@headlight.se>
2020-12-04 21:13:31 +0100

    wip

--- src/people/components/people.tsx
+++ src/people/components/people.tsx
@@ -8,15 +8,7 @@ const People: React.FC = () => {
   const [people, setPeople] = React.useState<IPerson[]>([]);
 
   const fetchPersons = async () => {
-    setIsFetching(true);
-    const response = await fetch("https://swapi.dev/api/people");
-
-    if (response.ok) {
-      const data: { results: IPerson[] } = await response.json();
-      setPeople(data.results);
-    }
-
-    setIsFetching(false);
+    // "https://swapi.dev/api/people"
   };
 
   React.useEffect(() => {
